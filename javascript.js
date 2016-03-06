9aa3cf697b710baf84e6847efdfe0a6ec1c5799b
William Li <lijianzhao@lijianzhao.com>
2016-03-05 19:36:36 -0700

    add thread - not done yet

--- javascript.js
+++ javascript.js
@@ -5,6 +5,47 @@ var verifiedAccountIcon='<img src="img/verified.png">';
 var commentObject={author:'Phong123'+verifiedAccountIcon,content:"I'm coming. Be there in 5"};
 commentList.push(commentObject);
 
+
+var threads = []
+
+$(document).ready(function(){
+    $("#addNewThread").click(checkCookie);
+});
+
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; "+expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+
+
 function displayComment(popupContentArea){
 	
 	for (var i=0;i<commentList.length;i++){
